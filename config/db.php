b77d00b932306917c99f5b97c68086453af046df
Kelver Macahdo <kelver_kmv@hotmail.com>
2017-06-25 23:39:44 -0400

    Alterando banco

--- config/db.php
+++ config/db.php
@@ -5,14 +5,14 @@
  * Date: 19/05/17
  * Time: 23:32
  */
-
+//mysql://b06ff4aebd980e:ec8e55a0@us-cdbr-iron-east-03.cleardb.net/heroku_a2c4859427bd568?reconnect=true
 return [
     "development" => [
         'driver' => 'mysql',
-        'host' => 'localhost',
-        'database' => 'plantasEmpire',
-        'username' => 'root',
-        'password' => 'root',
+        'host' => 'us-cdbr-iron-east-03.cleardb.net',
+        'database' => 'heroku_a2c4859427bd568',
+        'username' => 'b06ff4aebd980e',
+        'password' => 'ec8e55a0',
         'charset' => 'utf8',
         'collation' => 'utf8_unicode_ci'
     ]
