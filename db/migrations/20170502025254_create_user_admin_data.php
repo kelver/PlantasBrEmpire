e0c914e217c6a9084a50b76873d333ae014112a8
Kelver Macahdo <kelver_kmv@hotmail.com>
2017-07-01 00:16:22 -0400

    Ajustes migration add cadastro para inserir o admin primário.

--- db/migrations/20170502025254_create_user_admin_data.php
+++ db/migrations/20170502025254_create_user_admin_data.php
@@ -9,16 +9,20 @@ class CreateUserAdminData extends AbstractMigration
        $app = require __DIR__ . '/../bootstrap.php';
        $auth = $app->service('auth');
 
-       $cadastros = $this->table('cadastro');
-        $cadastros->insert([
-            'usuario' => 'kelver',
-            'senha' => $auth->hashPassword('123456'),
-            'primeiro_acesso' => date('Y-m-d H:i:s'),
-            'ultimo_acesso' => date('Y-m-d H:i:s'),
-            'status' => 1,
-            'idPessoa' => 1,
-            'tipo' => 0 // 0 usuario comum, 1 usuário adm
-        ])->save();
+       $i='';
+       foreach(range(1,2) as $value) {
+           $cadastros = $this->table('cadastro');
+           $cadastros->insert([
+               'usuario' => 'kelver'.$i,
+               'senha' => $auth->hashPassword('123456'),
+               'primeiro_acesso' => date('Y-m-d H:i:s'),
+               'ultimo_acesso' => date('Y-m-d H:i:s'),
+               'status' => 1,
+               'idPessoa' => 1,
+               'tipo' => ($i=='')?'':1 // 0 usuario comum, 1 usuário adm
+           ])->save();
+           $i=2;
+       }
    }
 
    public function down()
